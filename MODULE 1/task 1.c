ae1a199c035c518815441dee02cd5f9970b17224
aayushgevariya <aayushgevariya22@gmail.com>
2025-07-15 11:00:24 +0530

    Add files via upload

---
+++ MODULE 1/task 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+
+printf("\n NAME       : AAYUSH GEVARIYA");
+printf("\n BIRTH DATE : 22-02-2007");
+printf("\n AGE        : 18");
+printf("\n ADDRESS    : RAJKOT");
+
+getch();
+}
